abc05d0036e6a8b5f19dcb18f1c7517abb68eb72
rekeye <12szymon.paluch34@gmail.com>
2023-06-03 17:09:13 +0200

    Add avatar generator

--- src/routes/welcome.tsx
+++ src/routes/welcome.tsx
@@ -1,13 +1,21 @@
+import { createSignal } from "solid-js";
+import { v4 as uuidv4 } from "uuid";
+import { AvatarGenerator } from "~/components/AvatarGenerator";
 import Protected from "~/components/Protected";
 
 export const { routeData, Page } = Protected((session) => {
 	const user = session.user
+	const [avatarId, setAvatarId] = createSignal(uuidv4())
 
-  return (
-    <main class="grow flex flex-col items-center justify-center">
-      <div class="my-4">Hello {user?.name}</div>
-    </main>
-  );
+	return (
+		<main class="grow flex flex-col items-center justify-center">
+			<h1 class="py-4 text-2xl font-semibold">Welcome</h1>
+			<form class="flex flex-col items-center">
+			<h2 class="py-2 text-xl font-medium">Select your avatar</h2>
+				<AvatarGenerator url={`https://source.boringavatars.com/beam/120/${avatarId()}`} onRegenerate={() => setAvatarId(uuidv4())} />
+			</form>
+		</main>
+	);
 })
 
 export default Page;
