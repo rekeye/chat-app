41728b7af163dc737987c6978168f5ad5d6cf800
rekeye <12szymon.paluch34@gmail.com>
2023-06-03 16:39:27 +0200

    Add header for logout button

--- src/routes/welcome.tsx
+++ src/routes/welcome.tsx
@@ -1,16 +1,11 @@
-import { signOut } from "@auth/solid-start/client";
-import { Button } from "~/components/Button";
 import Protected from "~/components/Protected";
 
 export const { routeData, Page } = Protected((session) => {
 	const user = session.user
 
-	const handleLogout = () => signOut() 
-
   return (
     <main class="grow flex flex-col items-center justify-center">
       <div class="my-4">Hello {user?.name}</div>
-			<Button text="Logout" onClick={handleLogout} />
     </main>
   );
 })
